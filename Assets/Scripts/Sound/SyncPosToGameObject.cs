c8be99eaf628e5224302302bec579d4284b0f5af
Paul Mieschke <p.mieschke@gmail.com>
2019-07-20 19:21:15 +0200

    Added possibility to sync movement to camera -> ambient sounds

---
+++ Assets/Scripts/Sound/SyncPosToGameObject.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class SyncPosToGameObject : MonoBehaviour {
+    public GameObject gameObjectToFollow;
+    public bool x = false, y = false, z = false;
+
+    private Vector3 newOwnPos;
+
+    private void LateUpdate() {
+        newOwnPos = this.gameObject.transform.position;
+        if (x) newOwnPos.x = gameObjectToFollow.transform.position.x;
+        if (y) newOwnPos.y = gameObjectToFollow.transform.position.y;
+        if (z) newOwnPos.z = gameObjectToFollow.transform.position.z;
+        this.gameObject.transform.position = newOwnPos;
+    }
+}
