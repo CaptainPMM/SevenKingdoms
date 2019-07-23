dd1bd2c15871d00e0ba0a0d07a1d96021b3f654b
Paul Mieschke <p.mieschke@gmail.com>
2019-07-23 03:33:07 +0200

    Started menu

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+    public void LoadSceneByName(string name) {
+        SceneManager.LoadScene(name, LoadSceneMode.Single);
+    }
+}
