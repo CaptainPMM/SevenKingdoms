903e1fbececda857ad3ef1d46fa457b016c0606f
Paul Mieschke <p.mieschke@gmail.com>
2019-07-26 12:01:18 +0200

    Mobile camera finished

--- Assets/Scripts/Camera/CameraControllerMOBILE.cs
+++ Assets/Scripts/Camera/CameraControllerMOBILE.cs
@@ -5,6 +5,8 @@ public class CameraControllerMOBILE : MonoBehaviour {
     public float scrollingSpeed;
     public float scrollHeightModifier = 0.1f;
 
+    private bool dragging = false;
+
     private void Awake() {
         cameraSpeed = Global.CAMERA_SPEED;
         scrollingSpeed = Global.CAMERA_ZOOM_SPEED;
@@ -12,6 +14,32 @@ public class CameraControllerMOBILE : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        // MOVING
+        if (Input.touchCount == 1) {
+            Touch t = Input.GetTouch(0);
+            if (t.phase == TouchPhase.Began) {
+                Ray ray = Camera.main.ScreenPointToRay(t.position);
+                RaycastHit hit;
+
+                // Check for hit valid game objects
+                if (Physics.Raycast(ray, out hit)) {
+                    if (hit.collider.gameObject.tag == "background") {
+                        dragging = true;
+                    }
+                }
+            }
+
+            if (dragging && t.phase == TouchPhase.Moved) {
+                Vector2 movement = t.position - t.deltaPosition;
+
+                transform.position += new Vector3(movement.x, 0, movement.y) * cameraSpeed * CalculateScrollHeightModifier() * Time.deltaTime;
+
+                Global.LimitCameraToBoundaries(this);
+            }
+
+            if (dragging && t.phase == TouchPhase.Ended) dragging = false;
+        }
+
         // ZOOMING / SCROLLING
         if (Input.touchCount == 2) {
             Touch touch0 = Input.GetTouch(0);
