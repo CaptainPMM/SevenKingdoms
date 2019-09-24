02d5991512b9083d0652472593e7ca9f426edfc0
Paul Mieschke <p.mieschke@gmail.com>
2019-09-24 22:15:12 +0200

    Small text adjustment

--- Assets/Scripts/Buildings/OuterTownRing.cs
+++ Assets/Scripts/Buildings/OuterTownRing.cs
@@ -2,7 +2,7 @@ public class OuterTownRing : Building {
     public OuterTownRing() {
         buildingType = BuildingType.OUTER_TOWN_RING;
         _buildingName = "Outer Town Ring";
-        _description = "The town has to grow. Build an outer town ring around the perimeter. Increases manpower.";
+        _description = "The town has to grow. Build an outer town ring around the perimeter. Increases Manpower income.";
         _neededGold = 600;
         _gameEffects = new GameEffect[] {
             GameEffect.MANPOWER_INCOME_MODIFIER_HIGH
