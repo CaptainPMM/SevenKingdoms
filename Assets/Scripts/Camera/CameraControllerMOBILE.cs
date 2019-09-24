7fa1422338696a671d2ae3b2118c1dffd4e10702
Paul Mieschke <p.mieschke@gmail.com>
2019-09-24 23:44:14 +0200

    Fixed mobile camera

--- Assets/Scripts/Camera/CameraControllerMOBILE.cs
+++ Assets/Scripts/Camera/CameraControllerMOBILE.cs
@@ -6,50 +6,53 @@ public class CameraControllerMOBILE : MonoBehaviour {
     public float scrollHeightModifier = 0.1f;
 
     private Vector3 touchStart;
+    private bool touching;
 
     private void Awake() {
         scrollingSpeed = Global.CAMERA_ZOOM_SPEED * 0.03f; // Adjust mobile zoom speed
+        touching = false;
     }
 
     // Update is called once per frame
     void Update() {
         if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId)) {
+            touching = true;
             touchStart = GetWorldPosition();
         }
 
-        // ZOOMING / SCROLLING
-        if (Input.touchCount == 2) {
-            Touch touch0 = Input.GetTouch(0);
-            Touch touch1 = Input.GetTouch(1);
+        if (Input.touchCount == 0) touching = false;
 
-            if (!EventSystem.current.IsPointerOverGameObject(touch0.fingerId) && !EventSystem.current.IsPointerOverGameObject(touch1.fingerId)) {
-                // Find the position in the previous frame of each touch
-                Vector2 touch0PrevPos = touch0.position - touch0.deltaPosition;
-                Vector2 touch1PrevPos = touch1.position - touch1.deltaPosition;
+        if (touching) {
+            // ZOOMING / SCROLLING
+            if (Input.touchCount == 2) {
+                Touch touch0 = Input.GetTouch(0);
+                Touch touch1 = Input.GetTouch(1);
 
-                // Find the magnitude/length of the vector (distance) between the touches in each frame
-                float prevTouchDeltaMag = (touch0PrevPos - touch1PrevPos).magnitude;
-                float touchDeltaMag = (touch0.position - touch1.position).magnitude;
+                if (!EventSystem.current.IsPointerOverGameObject(touch0.fingerId) && !EventSystem.current.IsPointerOverGameObject(touch1.fingerId)) {
+                    // Find the position in the previous frame of each touch
+                    Vector2 touch0PrevPos = touch0.position - touch0.deltaPosition;
+                    Vector2 touch1PrevPos = touch1.position - touch1.deltaPosition;
 
-                // Find the difference in the distances between each frame
-                float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+                    // Find the magnitude/length of the vector (distance) between the touches in each frame
+                    float prevTouchDeltaMag = (touch0PrevPos - touch1PrevPos).magnitude;
+                    float touchDeltaMag = (touch0.position - touch1.position).magnitude;
 
-                // Now its time to zoom/scroll the camera
-                transform.position += new Vector3(0, deltaMagnitudeDiff, 0) * scrollingSpeed * CalculateScrollHeightModifier() * Time.deltaTime;
+                    // Find the difference in the distances between each frame
+                    float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
 
-                // Also allow movement while zooming
-                Vector3 direction = touchStart - GetWorldPosition();
-                gameObject.transform.position += direction * 10f * CalculateScrollHeightModifier() * Time.deltaTime;
+                    // Now its time to zoom/scroll the camera
+                    transform.position += new Vector3(0, deltaMagnitudeDiff, 0) * scrollingSpeed * CalculateScrollHeightModifier() * Time.deltaTime;
 
-                Global.LimitCameraToBoundaries(this);
-            }
-        } else {
-            // MOVING
-            if (!GameController.activeGameController.dragging) {
-                if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId)) {
-                    Vector3 direction = touchStart - GetWorldPosition();
-                    gameObject.transform.position += direction;
                     Global.LimitCameraToBoundaries(this);
+                }
+            } else {
+                // MOVING
+                if (!GameController.activeGameController.dragging) {
+                    if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId)) {
+                        Vector3 direction = touchStart - GetWorldPosition();
+                        gameObject.transform.position += direction;
+                        Global.LimitCameraToBoundaries(this);
+                    }
                 }
             }
         }
